d2e84cd5ec04fe08176cf0d43ea8dfe886d28f13
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-07 00:17:40 -0300

    :tada: Add project

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTables";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
