8fdafec592452eff598e222c34a7e2ab0595d96a
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-07 22:31:11 -0300

    :tada: Add project

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.div`
   h2 {
@@ -55,13 +55,24 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: "green" | "red";
 }
+
+const colors = {
+  green: "#33cc96",
+  red: "#e52e4d",
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: transparent;
+
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
+
   display: flex;
   align-items: center;
   justify-content: center;
